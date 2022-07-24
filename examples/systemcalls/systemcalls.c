94877b019f73dd82980922dafcea633fab1404bb
jarvis <182097228@qq.com>
2022-07-24 21:17:12 +0800

    assig 3-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +22,12 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret = system(cmd);
+    
+    if(ret != 0 )
+    {
+    	return false;
+    }
 
     return true;
 }
@@ -58,10 +70,26 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	int child_status;
+	pid_t terminate_pid;
+	pid_t child_pid = fork();
+	if(child_pid == 0)
+	{
+		exit((execv(command[0],command)== -1)? -1 : 0);
+	}
+	else
+	{
+		do
+		{
+			terminate_pid = wait(&child_status);
+		}
+		while(terminate_pid != child_pid);
+	}
 
     va_end(args);
 
-    return true;
+    return (child_status == 0)? true : false;
+
 }
 
 /**
@@ -92,8 +120,48 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+ int child_status;
+    pid_t terminate_pid;
+    pid_t child_pid = fork();
+
+    if (child_pid == 0)  // Child process
+    {
+        int ret = -1;
+        int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+
+        if (fd < 0) 
+        {
+            perror("open");
+        }
+        else
+        {
+            // Using dup2() to redirect output
+            // https://www.cs.utexas.edu/~theksong/2020/243/Using-dup2-to-redirect-output/
+
+            if (dup2(fd, STDOUT_FILENO) < 0) 
+            {
+                perror("dup2");
+            }
+            else
+            {
+                ret = (execv(command[0], command) == -1)? -1 : 0;
+            }
+        }
+
+        close(fd);
+        exit(ret);
+    }
+    else  // Parent process
+    {
+        do
+        {
+            terminate_pid = wait(&child_status);
+        }
+        while (terminate_pid != child_pid);
+    }
+
 
     va_end(args);
 
-    return true;
+    return (child_status == 0)? true : false;
 }
